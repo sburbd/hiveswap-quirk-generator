a64adf2528f7c1b265d15c05dca2b0271b9f0d1c
Zichao Zang <zichaozang@gmail.com>
2018-12-05 14:01:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("resize", mobileCheck);
 
 function mobileCheck() {
   if (window.innerWidth <= 700) {
-     window.location = "/mobile.html"
+     window.location = "/hiveswap-quirk-generator/mobile.html"
    } else {
     window.location = "/"
  }
